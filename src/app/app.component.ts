461a36862de6496b4a9348dbc265772d0d4b7d47
Fahad Billah <fahadbillah@yahoo.com>
2019-12-18 23:36:11 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iprice';
+  showUpperCase: String;
+  showAlterCase: String;
+  showCSV: any;
+
+  constructor(private sanitizer: DomSanitizer){ }
+
+  onSubmit(text) {
+    console.log(text, 'test');
+    const input = !!text.input ? text.input : "hello world";
+    
+    this.makeUppercase(input);
+    this.makeAltercase(input);
+    this.makeCSV(input);
+  }
+
+  makeUppercase(input: String): void{
+    this.showUpperCase = input.toUpperCase();
+  }
+
+  makeAltercase(input: String): void{
+    this.showAlterCase = '';
+    input.split('').forEach((char, idx)=>{
+      this.showAlterCase += (idx % 2 === 0) ? char.toUpperCase() : char.toLowerCase();
+    });
+    console.log(this.showAlterCase);
+  }
+
+  makeCSV(input: String): void{
+    this.showCSV = '';
+    const blob = new Blob([input.split(' ').join(',')], {type : 'text/csv;charset=utf-8;'})
+    this.showCSV = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+    console.log(blob, this.showCSV)
+  }
+
 }
